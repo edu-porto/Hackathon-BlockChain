bb23dc13a65fb6711fd00ac9a89ce54971735395
Pedro Gattai <Pedro.Gattai@sou.inteli.edu.br>
2023-05-06 13:58:11 -0300

    Revert "fit"
    This reverts commit 40e32c8b85cbab3c2fd340243187503cfb8b8b7b.

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,41 @@
-const express = require('express');
+<<<<<<< HEAD
 const { PythonShell } = require('python-shell');
-
-const app = express();
-const port = 3000;
 
 let coin = 'bitcoin';
 
 let options = {
+    mode: 'text',
     pythonPath: 'C:/Program Files/WindowsApps/PythonSoftwareFoundation.Python.3.9_3.9.3568.0_x64__qbz5n2kfra8p0/python3.9.exe', // Replace with the path to your python installation
+    pythonOptions: ['-u'],
     scriptPath: '', // Replace with the path to your python script
     args: [coin]
 };
 
-app.get('/report', (req, res) => {
-    PythonShell.run('coinGeckoAPI.py', options)
-        .then((result) => {
-            console.log('Python script execution completed.');
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log('An error occurred:', err);
-            res.send('Error occurred during Python script execution.');
-        });
+PythonShell.run('coinGeckoAPI.py', options, function (err, result) {
+    if (err) throw err;
+    let jsonResult = JSON.parse(result[0]);
+    console.log(jsonResult);
+=======
+const spawn = require("child_process").spawn;
+
+// Spawn a new Python process and pass in two arguments
+const pythonProcess = spawn('C:/Program Files/WindowsApps/PythonSoftwareFoundation.Python.3.9_3.9.3568.0_x64__qbz5n2kfra8p0/python3.9.exe', ["hello.py", "12", "2"]);
+
+// Listen for data on the stdout stream of the Python process
+pythonProcess.stdout.on('data', (data) => {
+    // Do something with the data returned from the Python script
+    console.log(data.toString());
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}.`);
+// Listen for any errors that occur while running the Python process
+pythonProcess.stderr.on('data', (data) => {
+    console.error(data.toString());
+>>>>>>> parent of 3e53e41 (Merge pull request #2 from edu-porto/backend)
 });
+
+// Listen for the Python process to exit
+pythonProcess.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
+
+console.log("Started Python process...");
