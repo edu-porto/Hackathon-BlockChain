ea671a6cfe2b29fa872d8f2e0ee2c4fa7bdbac45
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2023-05-07 06:37:04 -0300

    feat: add finished frontend

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from '../pages/landingPage'
 import Home from '../pages/home'
+import Header from '../components/partials/header'
 
 const Router: React.FC = () => {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path='/*' element={<Home />}></Route>
-        </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path='/*' element={<Home />}></Route>
+                <Route path='/buy' element={<LandingPage />}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default Router
